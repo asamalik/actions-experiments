44f66dc8dfc8c7a34fd434d3daf689a7a4810917
Adam Samalik <asamalik@redhat.com>
2021-02-05 14:55:13 +0100

    add a very simple test script

---
+++ test1.py
@@ -0,0 +1,22 @@
+
+
+def main():
+    
+    pr_dir = None
+    repo_dir = None
+
+    print("This PR is adding/changing {} files:".format(os.listdir(pr_dir)))
+
+    for filename in os.listdir(pr_dir):
+        print(filename)
+
+    print("")
+
+    print("The repo has {} files:".format(os.listdir(repo_dir)))
+
+    for filename in os.listdir(repo_dir):
+        print(filename)
+
+
+if __name__ == "__main__":
+    main()
